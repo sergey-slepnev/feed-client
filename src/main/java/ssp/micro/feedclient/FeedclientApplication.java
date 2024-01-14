d63d30f30df1701fcfd8a440c227e736cf7db615
sergey-slepnev <kolybelnaya27ss@gmail.com>
2024-01-14 20:05:39 +0800

    initial commit

---
+++ src/main/java/ssp/micro/feedclient/FeedclientApplication.java
@@ -0,0 +1,13 @@
+package ssp.micro.feedclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FeedclientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FeedclientApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ssp/micro/feedclient/FeedclientApplicationTests.java
@@ -0,0 +1,13 @@
+package ssp.micro.feedclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FeedclientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
